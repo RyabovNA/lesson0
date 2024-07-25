532c87d7dd1664113733c3474d6f0abfb786e41e
RyabovNA <kulek19862009@mail.ru>
2024-07-25 23:52:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#"1st program".
+print(9 ** 0.5 * 5)
+
+#"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#"3rd program"
+print((1234 % 1000 // 10) + (5678 % 1000 // 10))
+
+#"4th program"
+print(((int((round(13.42 % 1, 2) * 100))) == int (42.13)) or ((int((round(42.13 % 1, 2) * 100))) == int (13.42)))
